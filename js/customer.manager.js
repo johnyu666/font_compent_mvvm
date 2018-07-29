9aef9a60d90550e61b8ead687dc6c62ba0c1569c
yuyinghan@yeah.net <yuyinghan@yeah.net>
2018-07-29 19:56:40 +0800

    服务器端实现跨域，前端代码进一步优化

--- js/customer.manager.js
+++ js/customer.manager.js
@@ -1,3 +1,11 @@
+/**
+ * 用于完成对Ajax的通用封装
+ * @param url
+ * @param method
+ * @param data
+ * @param callback
+ * @returns
+ */
 function myAjax(url,method,data,callback){
     let contentType="application/json";
     let options={url:url,type:method,contentType:contentType};
@@ -7,30 +15,75 @@ function myAjax(url,method,data,callback){
             callback(obj);
         })
 }
-
+/**
+ * 用于将form中的域转换为JSON
+ * @param form
+ * @returns
+ */
 function form2json(form) {
     let obj={};
     $.each(form.children("[type!=submit]"),(index,item)=>{
         let $item=$(item);
-        obj[[$item.attr("name")]]=$item.val();
+        obj[$item.attr("name")]=$item.val();
 
     })
     return obj;
 }
+/**
+ * 采用观察者模式，完成了模型，可用于通用项目
+ * @param comp
+ * @returns
+ */
+function Model(comp) {
+    Array.call(this);
+    if(typeof this.add!='function'){
+        Model.prototype.add=function (c) {
+            this.push(c)
+            comp.render();
+        }
+    }
+    if(typeof this.urd!='function'){
+        Model.prototype.urd=function () {
+            [].splice.apply(this,arguments);
+            comp.render();
+        }
+    }
+    if(typeof this.pushArray!='function'){
+        Model.prototype.pushArray=function () {
+            [].push.apply(this,arguments[0]);
+            comp.render();
+        }
+    }
+}
+Model.prototype=new Array();
 
-function CustomerComponent(view,url) {
-    /*局部变量初始块*/
-    let model=null;
 
-    let tbody=view.find("tbody");
-    let updateForm=view.find("#updateForm");
-    let currentCustomer=null;
-    initBindEvent=initBindEvent.bind(this);
-    renderTable=renderTable.bind(this);
-
-    /*局部函数*/
-    function renderTable() {
-        tbody.empty();
+/**
+ * 本页面中的“容器性质”的Component
+ * @param $view
+ * @param url
+ * @returns
+ */
+function AppComponent($view,url) {
+    //闭包形式，公开私有函数，用于数据更新出现错误时，调用
+    this.refresh=refresh;
+    //mv的绑定点，实现观察者模式
+    let model=new Model(this);
+    let updateComponent=null;
+    new AddFormComponet($view.find("#addForm"),model,url);
+    updateComponent=new UpdateFormComponet($view.find("#updateForm"),model,url);
+    refresh();//此处无法使用this指针，所以需要用闭包公开
+    //前端数据洗白
+    function refresh(){
+        myAjax(url,"GET",null, data=> {
+            model.length=0;
+            model.pushArray(data);
+        });
+    }
+    //此render用于实现：观察者模式中的“Observer接口的接收通知方法”
+    this.render=function () {
+        let $tbody=$view.find("tbody");
+        $tbody.empty();
         model.forEach((item,index)=>{
             $("<tr>")
                 .append($("<td>").text(item.id))
@@ -39,74 +92,57 @@ function CustomerComponent(view,url) {
                     this.delete(item);
                 }))
                 .on("dblclick",(e)=>{
-                    this.selectCustomer(item);
+                    //将更新表单重新渲染
+                    updateComponent.render(item);
                 })
-                .appendTo(tbody);
+                .appendTo($tbody);
         });
     }
 
-    function initBindEvent() {
-        $("#addForm").on("submit",(e)=>{
-            e.preventDefault();
-            let data=form2json($(e.target));
-            myAjax(url,"POST",data,(c)=>{
-                this.addCustomer(c);
-            })
-
-        })
-        $("#updateForm").on("submit",(e)=>{
-            e.preventDefault();
-            let data=form2json($(e.target));
-            myAjax(url+currentCustomer.id,"PUT",data,(c)=>{
-                this.updateCutomer(c);
-            })
-        })
-    }
-
-
-    this.init=function () {
-        initBindEvent();
-        myAjax(url,"GET",null, data=> {
-            model=data;
-            this.render();
-        });
-    }
-    this.render=function () {
-        renderTable();
-    }
-
-
-    this.selectCustomer=function (item) {
-        currentCustomer=item;
-        let cname=updateForm.children("[name=cname]").val(currentCustomer.cname);
-    }
-    this.updateCutomer=function (item) {
-        myAjax(url+item.id,"PUT",item,customer=>{
-            let index=model.indexOf(currentCustomer);
-            model.splice(index,1,customer);
-            this.render();
-        });
-    };
     this.delete=function (item) {
         myAjax(url+item.id,"DELETE",null,data=>{
             if(data.error){
                 alert("删除失败");
-                this.init();
+                this.refresh();
             }
             else{
                 let index=model.indexOf(item);
-                model.splice(index,1);
-                this.render();
+                model.urd(index,1);
             }
         });
     }
-    this.addCustomer=function (customer) {
-        model.push(customer);
-        this.render();
+}
+
+//添加表单组件
+function AddFormComponet($view,model,url) {
+    $view.on("submit",(e)=>{
+        e.preventDefault();
+        let data=form2json($(e.target));
+        myAjax(url,"POST",data,(c)=>{
+            model.add(c);
+        })
+    })
+}
+//更新表单组件
+function UpdateFormComponet($view,model,url) {
+    let cur=null;
+    // 为更新表单添加提交事件处理
+    $view.on("submit",(e)=>{
+        e.preventDefault();
+        let data=form2json($(e.target));
+        myAjax(url+cur.id,"PUT",data,(c)=>{
+            let index=model.indexOf(cur);
+            console.log(index)
+            model.urd(index,1,c);
+        })
+    })
+
+    this.render=function (item) {
+        cur=item;
+        $view.find("[name=cname]").val(item.cname);
     }
 }
 
 $(function () {
-    let component=new CustomerComponent($("#app"),"http://localhost:3000/customers/");
-     component.init();
+    let app=new AppComponent($("#app"),"http://localhost:8080/integrator/customers/");
 });
